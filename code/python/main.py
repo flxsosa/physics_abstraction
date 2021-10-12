0dc53c0a73bd31735bae83bfc135acc8760b96e1
Felix Sosa <felixanthonysosa@gmail.com>
2021-10-12 19:11:06 -0400

    added accesible properties to use in other functions that might need the info. Commented out useless variables

--- code/python/main.py
+++ code/python/main.py
@@ -10,12 +10,12 @@ fps = 60
 width, height = 800,800
 space = pymunk.Space()
 space.gravity = 0,150
-pygame.init()
-screen = pygame.display.set_mode((width,height))
-clock = pygame.time.Clock()
+# pygame.init()
+# screen = pygame.display.set_mode((width,height))
+# clock = pygame.time.Clock()
 running = True
 step = False
-draw_options = pymunk.pygame_util.DrawOptions(screen)
+# draw_options = pymunk.pygame_util.DrawOptions(screen)
 
 class GameBorder:
     def __init__(self, space, p0=(10, 10), p1=(width-10, height-10), d=2):
@@ -29,24 +29,25 @@ class GameBorder:
             space.add(segment)
 
 class Container:
-    def __init__(self, space, pos=(width/2,height/2), l=80,d=2):
-        self.pos = pos
+    def __init__(self, pos=(width/2,height/2), l=80,d=2):
+        self.init_position = pos
+        self.body = pymunk.Body(body_type=pymunk.Body.STATIC)
         x,y = pos
-        b_seg_y = y+l
-        b_seg_x1 = pos[0]+l/2
-        b_seg_x2 = pos[0]-l/2
-        bottom_segment = pymunk.Segment(space.static_body, (b_seg_x1,b_seg_y), (b_seg_x2,b_seg_y),d)
-        l_seg_x1 = pos[0] - l/2
-        l_seg_y1 = y+l
-        l_seg_x2 = pos[0] - l/2
-        l_seg_y2 = y
-        left_segment = pymunk.Segment(space.static_body, (l_seg_x1,l_seg_y1), (l_seg_x2,l_seg_y2),d)
-        r_seg_x1 = pos[0] + l/2
-        r_seg_y1 = y+l
-        r_seg_x2 = pos[0] + l/2
-        r_seg_y2 = y
-        right_segment = pymunk.Segment(space.static_body, (r_seg_x1, r_seg_y1), (r_seg_x2, r_seg_y2),d)
-        space.add(bottom_segment, left_segment,right_segment)
+        b_y = y+l
+        b_x1 = x+l/2
+        b_x2 = x-l/2
+        b_segment = pymunk.Segment(self.body,(b_x1,b_y),(b_x2,b_y),d)
+        l_y1 = y+l
+        l_y2 = y
+        l_x1 = x - l/2
+        l_x2 = x - l/2
+        l_segment = pymunk.Segment(self.body,(l_x1,l_y1),(l_x2,l_y2),d)
+        r_y1 = y+l
+        r_y2 = y
+        r_x1 = x + l/2
+        r_x2 = x + l/2
+        r_segment = pymunk.Segment(self.body,(r_x1, r_y1),(r_x2, r_y2),d)
+        self.components = self.body,b_segment,l_segment,r_segment
 
 class Platform:
     def __init__(self, space, pos=(width/2,height/2), l=80,d=2):
@@ -83,9 +84,10 @@ class Goal:
         self.shape = pymunk.Poly.create_box(self.body, (l, l/2))
         self.shape.color = pygame.Color("green")
         self.shape.collision_type = 1
+        self.components = [self.body,self.shape]
 
 class Ball:
-    def __init__(self, space, pos=(width/2,height/2), r=20):
+    def __init__(self, pos=(width/2,height/2), r=20):
         self.init_position = pos
         self.init_radius = r
         self.body = pymunk.Body(10.0, 1)
@@ -94,6 +96,7 @@ class Ball:
         self.shape = pymunk.Circle(self.body, r)   
         self.shape.color = pygame.Color("red")
         self.shape.collision_type = 0
+        self.components = self.body,self.shape
 
 class Teleporter:
     def __init__(self, space, pos=(width/2,height/2), mask=2, l=80):
