cf251f4096a35b349438085ac14d0f05ef804f83
Felix Sosa <felixanthonysosa@gmail.com>
2021-10-25 11:08:45 -0400

    renamed

--- code/python/handlers.py
+++
@@ -1,7 +0,0 @@
-from pygame.locals import *
-
-def teleporter(arbiter, space, data):
-    a = space.bodies[-1]
-    b = space.bodies[-2]
-    a.position = b.position[0], b.position[1]+41
-    return True
