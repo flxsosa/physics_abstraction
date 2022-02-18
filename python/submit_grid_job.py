6d26ba160314c9a1af5795d7e1ac4cd84c2e2cdf
Felix Sosa <lollipop@Felixs-MacBook-Pro.local>
2022-02-18 10:56:03 -0500

    setup for slurm

--- python/submit_grid_job.py
+++ python/submit_grid_job.py
@@ -13,21 +13,22 @@ def main():
     # Range for parameter D -- (5,11), ..., (40,46)
     n_ranges = [(n*5, (n+1)*5+1) for n in range(1,9)]
     
-    # # Submit a job per parameter setting
-    print(len([(n,d) for n in n_ranges for d in d_ranges]))
+    # Submit a job per parameter setting
     for param_setting in ((n,d) for n in n_ranges for d in d_ranges):
+        # N and d parameter ranges
         n, d = param_setting
         # Get the filename for storage
         fname = f"grid_fits_n{n[0]}_{n[1]}_d{d[0]}_{d[1]}"
-        print(fname)
-        # # The sbatch command
+        # The sbatch command
         submit_command = (
             "sbatch " + # sbatch command
             # Job name
             f"--job-name={fname} " +
+            # Export relevant variables to jobscript
             f"--export=NRANGE={n},DRANGE={d},FNAME={fname} " +
-            # The job script!
+            # The job script
             args.jobscript)
+        # Execute sbatch command
         exit_status = subprocess.call(submit_command,shell=True)
         # Check to make sure job submitted
         if exit_status is 1:
