c1584a814194a88090122886f5ce58f6f2e52a35
Felix Sosa <lollipop@dhcp-10-250-147-244.harvard.edu>
2022-03-07 10:10:10 -0500

    cleaned up and commented

--- python/helper.py
+++ python/helper.py
@@ -1,4 +1,4 @@
-from math import cos, sin
+from math import cos, sin, radians, pi
 import numpy as np
 import pygame
 # import cv2
@@ -6,11 +6,24 @@ import numpy as np
 
 
 def rotate(body,point):
+    '''
+    Rotates a pymunk body around a point
+
+    :param body: Body to rotate
+    :param point: Point to rotate body around
+    '''
     rotatedx = point[0] * cos(body.angle) - point[1] * sin(body.angle)
     rotatedy = point[0] * sin(body.angle) + point[1] * cos(body.angle)
     return (rotatedx, rotatedy)
 
 def draw_scanner(screen,body,scale):
+    '''
+    Draws a scanner object on screen using Pygame
+
+    :param screen: Screen to render scanner to
+    :param body: Body of scanner
+    :param scale: Scale of drawing
+    '''
     mx,my = body.position + np.multiply(body.velocity.normalized(), scale)
     pygame.draw.line(screen, pygame.Color("green"), (mx,my),body.position)
 
@@ -18,7 +31,9 @@ def make_video(screen,scene_name,dir):
     '''
     Generates screenshots from a simulation
 
-    screen::screen -- a pygame screen on which the simulation is rendered
+    :param screen: Pygame screen on which the simulation is rendered
+    :param scene_name: Name of the scene
+    :param dir: Directory to which to save video
     '''
     print(f"Passed Image Save Dir: {dir}")
     print(f"Passed Image Save Name: {scene_name}")
@@ -32,15 +47,16 @@ def make_video(screen,scene_name,dir):
         yield
 
 def vid_from_img(scene_name,dir,filetype="*.jpg"):
+    '''
+    Takes images generated from make_video and stitches them into a video
+    [DEPRECATED]
+    :param scene_name: Name of the simulation for file naming
+    :param filetype: Glob parameter
+    :param dir: Location to save video to
+    '''
     pass
 #     print(f"Passed movie directory: {dir}")
 #     print(f"Passed movie Scene Name: {scene_name}")
-#     '''
-#     Takes images generated from make_video and stitches them into a video
-
-#     name::str -- name of the simulation for file naming
-#     dir::str  -- parameter for globbing the files together
-#     '''
 #     img_dic = {}
 #     img_str = []
 #     size = 0,0
@@ -63,6 +79,11 @@ def vid_from_img(scene_name,dir,filetype="*.jpg"):
 def draw_circle_alpha(surface, color, center, radius):
     '''
     Draws a circle on the screen with variable alpha values
+
+    :param surface: Surface to draw to
+    :param color: Color of the shape
+    :param center: Center point of shape on screen
+    :param radius: Radius of shape
     '''
     target_rect = pygame.Rect(center, (0, 0)).inflate((radius * 2, radius * 2))
     shape_surf = pygame.Surface(target_rect.size, pygame.SRCALPHA)
@@ -70,6 +91,14 @@ def draw_circle_alpha(surface, color, center, radius):
     surface.blit(shape_surf, target_rect)
 
 def draw_application(surface, color, center, radius):
+    '''
+    Draw the point at which an abstraction was applied and accepted
+
+    :param surface: Surface to draw to
+    :param color: Color of the shape
+    :param center: Center point of shape on screen
+    :param radius: Radius of shape
+    '''
     pygame.draw.line(surface, color, 
                      (center[0]-radius,center[1]-radius),(center[0]+radius,center[1]+radius),
                      4)
@@ -80,7 +109,9 @@ def draw_application(surface, color, center, radius):
 def adjust_body_angle(object):
     '''
     Adjusts the object body's angle to point in the velocity vector's direction
+
+    :param object: Object to be adjusted
     '''
     x,y = object.body.velocity.normalized()
     angle = np.arctan2(y,x)
-    object.body.angle = math.radians(180*angle/math.pi - 90)     
+    object.body.angle = radians(180*angle/pi - 90)     
