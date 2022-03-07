e44cd963ec74db076da180159ba5e8b3ae8a78d2
Felix Sosa <lollipop@dhcp-10-250-147-244.harvard.edu>
2022-03-07 10:04:01 -0500

    added argparser and comments

--- python/combine_csv.py
+++ python/combine_csv.py
@@ -1,8 +1,16 @@
 import pandas as pd
 import glob
+import argparse
 
-extension = "csv"
-data_dir = "pilot3/"
-all_fnames = [i for i in glob.glob(f"{data_dir}*.{extension}")]
-combined_csv = pd.concat([pd.read_csv(f) for f in all_fnames])
-combined_csv.to_csv(f"{data_dir}data.csv", index=False, encoding='utf-8-sig')
+def main():
+    parser = argparse.ArgumentParser(
+            description="Combine all CSVs in a diretory into one CSV")
+    parser.add_argument('datadir',help="Where the data is")
+    # Parse args
+    args = parser.parse_args()
+    extension = "csv"
+    data_dir = args.datadir
+    # Gather files
+    all_fnames = [i for i in glob.glob(f"{data_dir}*.{extension}")]
+    combined_csv = pd.concat([pd.read_csv(f) for f in all_fnames])
+    combined_csv.to_csv(f"{data_dir}data.csv", index=False, encoding='utf-8-sig')
