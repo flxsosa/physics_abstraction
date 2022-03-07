e246ea8945e98505139e2adcae8ffec867fec57b
Felix Sosa <lollipop@dhcp-10-250-147-244.harvard.edu>
2022-03-07 10:12:26 -0500

    added module boilerplate

--- python/combine_csv.py
+++ python/combine_csv.py
@@ -14,3 +14,6 @@ def main():
     all_fnames = [i for i in glob.glob(f"{data_dir}*.{extension}")]
     combined_csv = pd.concat([pd.read_csv(f) for f in all_fnames])
     combined_csv.to_csv(f"{data_dir}data.csv", index=False, encoding='utf-8-sig')
+
+if __name__ == "__main__":
+        main()
