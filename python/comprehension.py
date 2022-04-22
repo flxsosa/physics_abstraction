87b9fef8d4a9630f5217e0e3c5129cf9f85295f5
Felix Sosa <lollipop@Felixs-MacBook-Pro.local>
2022-04-22 10:40:17 -0400

    added main wrapper

--- python/comprehension.py
+++ python/comprehension.py
@@ -10,21 +10,24 @@ import random
 from helper import vid_from_img
 from pilot4 import make_video
 
-dir = "/Users/lollipop/projects/physics_abstraction/data/json/pilot4/"
-json_files = ['comp_0.json','comp_1.json','comp_2.json','comp_3.json','comp_4.json','comp_5.json']
-for file in json_files[0:3]:
-    fname = file.split(".")[0]
-    with open(dir+file, 'r') as f:
-        scene_args = json.loads(f.read())
-    # scene = load_scene_from_args(scene_args)
-    # scene.instantiate_scene()
-    # scene.run(True,f"")
-    make_video(file)
-    vid_from_img(fname,"/Users/lollipop/projects/physics_abstraction/experiments/experiment4/img/stimuli/comprehension/")
+def main():
+    dir = "/Users/lollipop/projects/physics_abstraction/data/json/pilot4/"
+    json_files = ['comp_0.json','comp_1.json','comp_2.json','comp_3.json','comp_4.json','comp_5.json']
+    for file in json_files[0:3]:
+        fname = file.split(".")[0]
+        with open(dir+file, 'r') as f:
+            scene_args = json.loads(f.read())
+        # scene = load_scene_from_args(scene_args)
+        # scene.instantiate_scene()
+        # scene.run(True,f"")
+        make_video(file)
+        vid_from_img(fname,"/Users/lollipop/projects/physics_abstraction/experiments/experiment4/img/stimuli/comprehension/")
 
-physics = Physics()
-graphics = Graphics()
-scene = Scene(physics,[PlinkoBorder(),BottomBorder()],graphics)
+    physics = Physics()
+    graphics = Graphics()
+    scene = Scene(physics,[PlinkoBorder(),BottomBorder()],graphics)
 
-scene.run()
+    scene.run()
 
+if __name__ == "__main__":
+    main()
