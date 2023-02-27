b1e88b6d01f14afab851ca8c503dc6d2944e5480
Felix Sosa <fsosa@boslogin02.rc.fas.harvard.edu>
2023-02-26 20:12:38 -0500

    commented out cv2 import for cluster use

--- python/helper.py
+++ python/helper.py
@@ -1,7 +1,7 @@
 from math import cos, sin, radians, pi
 import numpy as np
 import pygame
-import cv2
+#import cv2
 import numpy as np
 import os
 import glob
@@ -47,32 +47,6 @@ def make_video(screen,scene_name,dir):
         file_name = dir+scene_name+str_num[-3:]+".jpg"
         pygame.image.save(screen,file_name)
         yield
-
-def vid_from_img(scene_name,dir,filetype="*.jpg"):
-    '''
-    Takes images generated from make_video and stitches them into a video
-    [DEPRECATED]
-    :param scene_name: Name of the simulation for file naming
-    :param filetype: Glob parameter
-    :param dir: Location to save video to
-    '''
-    print(f"Passed movie directory: {dir}")
-    print(f"Passed movie Scene Name: {scene_name}")
-    img_dic = {}
-    img_str = []
-    size = 0,0
-    for filename in glob.glob(dir+filetype):
-        img = cv2.imread(filename)
-        h,w,l =img.shape
-        size = (img.shape[1],img.shape[0])
-        img_str.append(filename)
-        img_dic[filename] = img
-    out = cv2.VideoWriter(dir+scene_name+'.mp4', cv2.VideoWriter_fourcc(*'avc1'),60,size)
-    img_str.sort()
-    for i in img_str:
-        out.write(img_dic[i])
-    out.release()
-    os.system("rm "+ dir +"*.jpg")
 
 def draw_circle_alpha(surface, color, center, radius):
     '''
