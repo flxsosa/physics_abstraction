03c1a1d3c4a2ee8d075162f94343a764b943488b
Felix Sosa <felixanthonysosa@gmail.com>
2023-03-01 10:56:06 -0500

    added main for cluster

--- python/combine_csv.py
+++ python/combine_csv.py
@@ -2,6 +2,24 @@ import pandas as pd
 import glob
 import argparse
 
+def main_cluster():
+    '''
+    Combines csv files of model outputs computed on cluster.
+    Ignores headers are removes duplicate rows.
+    '''
+    parser = argparse.ArgumentParser(
+            description="Combine all CSVs in a diretory into one CSV")
+    parser.add_argument('datadir',help="Where the data is")
+    # Parse args
+    args = parser.parse_args()
+    extension = "csv"
+    data_dir = args.datadir
+    # Gather files
+    all_fnames = [i for i in glob.glob(f"{data_dir}*.{extension}")]
+    combined_csv = pd.concat([pd.read_csv(f, header=0) for f in all_fnames])
+    combined_csv.to_csv(f"{data_dir}model_fits.csv",index=False,encoding='utf-8-sig')
+    
+
 def main():
     parser = argparse.ArgumentParser(
             description="Combine all CSVs in a diretory into one CSV")
@@ -16,4 +34,4 @@ def main():
     combined_csv.to_csv(f"{data_dir}data.csv", index=False, encoding='utf-8-sig')
 
 if __name__ == "__main__":
-        main()
+        main_cluster()
